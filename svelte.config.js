178c45f7928f78c409347b4dd4cabd4142d0c66e
GIOPIYO <giovanniopiyo@yahoo.com>
2025-04-11 00:59:01 +0300

    adapter changed

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-static'; // Make sure this is the only adapter imported/used
+import adapter from '@sveltejs/adapter-auto'; // Make sure this is the only adapter imported/used
+import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'; // Or '@sveltejs/kit/vite' depending on setup
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,14 +7,7 @@ const config = {
   preprocess: vitePreprocess(), // Or your specific preprocessor
 
   kit: {
-    adapter: adapter({
-      // Options for adapter-static
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html', // Important for SPAs
-      precompress: false,
-      strict: true
-    }),
+    adapter: adapter(),
     // If you had paths: { base: '...' } keep it if needed
   }
 };
