f0c980016d12dc958a8cccec7d6f77bf372c86cf
Boniface Giovanni Opiyo <112946220+GIOPIYO@users.noreply.github.com>
2025-04-11 01:05:44 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import adapter from '@sveltejs/adapter-auto'; // Make sure this is the only adapter imported/used
-import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'; // Or '@sveltejs/kit/vite' depending on setup
 
 /** @type {import('@sveltejs/kit').Config} */
